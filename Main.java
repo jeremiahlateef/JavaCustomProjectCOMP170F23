54751d5028d6302d8ad8f307eabc16655ebb3eff
Jeremiah Lateef <jeremiahlat@gmail.com>
2023-11-18 18:26:19 -0600

    JSON file

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import org.json.*; //added a library to parse the JSON data
 
 public class Main {
    public static void main(String[] args) {
@@ -22,23 +21,14 @@ public class Main {
 
    }
 
-   public static void welcomeStatement() {
-      System.out.println("Hi! Welcome to Multicultural Menu!!");
-      System.out.println("We have curated recipes from various parts of the world for you to enjoy.");
-      System.out.println("Take this short quiz, and let's go on an exciting culinary journey.");
-      System.out.print("Type your name: ");
+   public static void welcomeStatement() throws FileNotFoundException {
+      Scanner170 input = new Scanner170(new File(welcomemessage.txt));
       String name = console.nextLine();
       System.out.println("Thanks" + name + "answer these three questions to get your personalised recipe list.");
    }
 
-   public static String cuisine(String responseOne) {
-      Scanner console = new Scanner(System.in);
-      System.out.println(
-            "There are so many places to choose from. We have narrowed it down to four choices. Click on the location of your choice.");
-      System.out.println("A. Africa");
-      System.out.println("B. Asia");
-      System.out.println("C. Carribean");
-      System.out.println("D. Europe");
+   public static String cuisine(String responseOne) throws FileNotFoundException {
+      Scanner170 input = new Scanner170(new File(cuisine.txt));
       String continent = console.nextLine();
       ;
       String answer = console.next();
@@ -54,37 +44,29 @@ public class Main {
       return responseOne;
    }
 
-   // create a method for cuisine
-   public static String allergies(String responseTwo) {
-
-      System.out.println("Do you have any allergies. If yes, pick one from the options if none, pick the last option.");
-      System.out.println("A. Dairy");
-      System.out.println("B. Nuts - Pinenuts, Peanuts, Walnuts, Cashwenuts, Hazelnuts");
-      System.out.println("C. Seafood- including shellfish");
-      System.out.println("No Allergies");
+   // create a method for allergies
+   public static String allergies(String responseTwo) throws FileNotFoundException {
+      Scanner170 input = new Scanner170(new File(allergies.txt));
       String allergies = console.nextLine();
-
       return responseTwo;
    }
 
-   // create a method for allergies
-   public static String preference(String responseThree) {
-
-      System.out.println("Last Question! Do you have any particular dietary preferences?");
-      System.out.println("A. Vegan");
-      System.out.println("B. Non-vegan");
+   // create a method for dietary preference
+   public static String preference(String responseThree) throws FileNotFoundException {
+      Scanner170 input = new Scanner170(new File(preference.txt));
       String preference = console.nextLine();
-
       return responseThree;
    }
-   // create a method that will
+   // create a method that will report the goodbye statement
 
 public static void main
 
-   goodbyestatement(){
-
-  System.out.println(name + " !. You have excellent taste, give us some time while we cook up your recipe selections.");
-  System.out.println("Thanks for exploring cuisines across the world! Happy Cooking."); 
+   goodbyestatement() throws FileNotFoundException {
+Scanner170 input = new Scanner170(new File(goodbyestatement.txt));
 }
 
 }
+  * 
+  * 
+  
+
