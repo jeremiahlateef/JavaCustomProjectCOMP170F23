7c3f1dee25127854fad85012b5aa5fa9e52d52f0
Jeremiah Lateef Oluwaseun <lateefjeremiah@gmail.com>
2023-11-26 10:53:40 -0600

    added the code for retrieving the data from the JSON

--- Main.java
+++ Main.java
@@ -13,6 +13,69 @@ import org.json.simple.parser.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+                // Parse the JSON file
+        Object jsonObj = new JSONParser().parse(new FileReader("./recipeData.json"));
+
+
+        // System.out.println("hi");
+        // Parse the JSON file
+        // Object jsonObj = new JSONParser().parse(new FileReader("./recipeData.json"));
+
+        //typecast the recipeObj to JSONObject
+
+        // Create a JSON Object
+        JSONObject recipeObj = (JSONObject) jsonObj;
+
+        //Get European address
+        Map continent = ((Map) recipeObj.get("European"));
+
+        //Create an array and get the meal array from the continent object
+        JSONArray recipeArr = (JSONArray) continent.get("meal");
+
+        //create an iterator for the meal array
+        Iterator<Object> mealIterator = recipeArr.iterator();
+
+        //Create a boolean variable to retrieve the vegan menu based on its truism or falsity.
+        boolean vegan = true;
+        //Use a while loop to iterate through the meal Array
+        while (mealIterator.hasNext()) {
+            //Create an object for the meal object
+            JSONObject menu = (JSONObject) mealIterator.next();
+
+
+            if (vegan) {
+                if ((Boolean) menu.get("vegan")) {
+                    //if true, prints vegan true value
+                    //if false, prints vegan false value
+                    JSONArray allergies = (JSONArray) ((JSONObject) ((JSONArray) menu.get("allergies")).get(0)).get("nuts");
+
+                    //System.out.println("Answer");
+                    Iterator<Object> finalMenuIterator = allergies.iterator();
+                    while (finalMenuIterator.hasNext()) {
+                        System.out.println(finalMenuIterator.next());
+                    }
+                    //System.exit(0);
+                }
+            } else {
+                if (!(Boolean) menu.get("vegan")) {
+                    //if true, prints vegan true value
+                    //if false, prints vegan false value
+                    JSONArray allergies = (JSONArray) ((JSONObject) ((JSONArray) menu.get("allergies")).get(0)).get("nuts");
+
+                    //System.out.println("Answer");
+                    Iterator<Object> finalMenuIterator = allergies.iterator();
+                    while (finalMenuIterator.hasNext()) {
+                        System.out.println(finalMenuIterator.next());
+                    }
+                    System.exit(0);
+                }
+            }
+
+//
+
+        }
+        System.exit(0);
+        
         // import Scanner that will be used to requests user input
         Scanner170 console = new Scanner170(System.in);
         welcomeStatement(console);
